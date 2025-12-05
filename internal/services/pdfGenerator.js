2ef3b21ea832d8fe65df4c01a31e883ad93852ed
fmaovs <freddymauriciovillanuevasilva@gmail.com>
2025-12-05 15:35:08 -0500

    add pdf generator

---
+++ internal/services/pdfGenerator.js
@@ -0,0 +1,16 @@
+const pdf = require('html-pdf');
+
+contenido = `
+<body>
+    <h1>Prueba de pdf</h1>
+    <table>
+        <thead>
+            <th>nombre</th>
+            <th>apellido</th>
+        </thead>
+        <tbody>
+            <tr>no se</tr>
+        </tbody>
+    </table>
+</body>
+`;
